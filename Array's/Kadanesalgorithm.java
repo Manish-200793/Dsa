0d739878d41d174efce00277e40c3acd2ecb2e66
Manish-200793 <manish93813@gmail.com>
2026-06-14 19:37:27 +0530

    Day2

---
+++ Array's/Kadanesalgorithm.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Kadanesalgorithm {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enthe the size of array: ");
+        int n=sc.nextInt();
+        int currentsum=0;
+        int maxsum=Integer.MIN_VALUE;
+        int[] nums=new int[n];
+        System.out.println("enter "+n+" elements");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++){
+            currentsum+=nums[i];
+            if(currentsum>maxsum){
+                maxsum=currentsum;
+            }
+            if(currentsum<0){
+                currentsum=0;
+            }
+        }
+        System.out.println(maxsum);
+        sc.close();
+    }
+}
