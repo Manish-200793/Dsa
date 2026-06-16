73327a42150e9f879ce04a93a992af77c764a012
Manish-200793 <manish93813@gmail.com>
2026-06-16 14:33:59 +0530

    Day2

--- Array's/TwoSum.java
+++ Array's/TwoSum.java
@@ -1,22 +1,33 @@
 import java.util.*;
+class solution{
+    public int[] result(int [] nums,int target){
+        int n=nums.length;
+         for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
 public class TwoSum {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter teh size os array: ");
+        System.out.println("enter the size os array: ");
         int n=sc.nextInt();
         System.out.println("enter the target: ");
         int target=sc.nextInt();
         int[] nums=new int[n];
+        System.out.println("enter the elements: ");
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    System.out.print("the indeixes are "+i+" "+j);
-                }
-            }
-        }
+        solution sol=new solution();
+        int[] ans=sol.result(nums,target);
+        System.out.println(Arrays.toString(ans));
+       
         sc.close();
     }
 }
